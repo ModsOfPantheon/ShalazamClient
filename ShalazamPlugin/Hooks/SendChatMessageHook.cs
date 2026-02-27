63a8ad39930ae1bd93c3ce384b2843b6e33a9bc2
ModsOfPantheon <194365524+ModsOfPantheon@users.noreply.github.com>
2026-02-27 18:52:52 +0000

    Add custom /track command for starting tracking

---
+++ ShalazamPlugin/Hooks/SendChatMessageHook.cs
@@ -0,0 +1,37 @@
+using HarmonyLib;
+using Il2Cpp;
+using Il2CppPantheonPersist;
+
+namespace ShalazamPlugin.Hooks;
+
+[HarmonyPatch(typeof(EntityClientMessaging.Logic), nameof(EntityClientMessaging.Logic.SendChatMessage), typeof(string), typeof(ChatChannelType))]
+public class SendChatMessageHook
+{
+    private static bool Prefix(EntityClientMessaging.Logic __instance, string message, ChatChannelType channel)
+    {
+        if (message == "/track")
+        {
+            ModMain.TrackOffensiveTarget();
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(EntityClientMessaging.Logic), nameof(EntityClientMessaging.Logic.RequestWhisper))]
+public class RequestWhisperHook
+{
+    private static bool Prefix(UIChatInput __instance, string targetPlayerName, string message)
+    {
+        if (message == "/track")
+        {
+            ModMain.TrackOffensiveTarget();
+
+            return false;
+        }
+
+        return true;
+    }
+}
