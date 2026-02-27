9d757aa105d343a81d9803b8aacbcff4de986602
ModsOfPantheon <194365524+ModsOfPantheon@users.noreply.github.com>
2026-02-27 18:52:29 +0000

    Track entity with configurable distance

--- ShalazamPlugin/ModMain.cs
+++ ShalazamPlugin/ModMain.cs
@@ -1,6 +1,8 @@
 ﻿using Il2Cpp;
+using Il2CppPantheonPersist;
 using MelonLoader;
 using ShalazamPlugin.SDK;
+using UnityEngine;
 
 namespace ShalazamPlugin;
 
@@ -12,6 +14,7 @@ public class ModMain : MelonMod
     {
         var category = MelonPreferences.CreateCategory("ShalazamApi");
         var apiKey = category.CreateEntry<string>("ApiKey", "");
+        Globals.MinimumTrackingDistance = category.CreateEntry("MinimumTrackingDistance", 3f).Value;
 
         category.SaveToFile(false);
         
@@ -23,6 +26,30 @@ public class ModMain : MelonMod
 
     public override void OnApplicationQuit()
     {
+    }
+
+    public override void OnUpdate()
+    {
+        if (Globals.TrackedOffensiveEntity == null)
+        {
+            return;
+        }
+
+        var currentTargetPos = Globals.TrackedOffensiveEntity.transform.position;
+        
+        if (Globals._lastPosition == null)
+        {
+            Globals._lastPosition = Globals.TrackedOffensiveEntity.transform.position;
+            ShalazamClient.PostMonster(Globals.TrackedOffensiveEntity);
+
+            return;
+        }
+
+        if (Vector3.Distance(currentTargetPos, Globals._lastPosition.Value) > Globals.MinimumTrackingDistance)
+        {
+            ShalazamClient.PostMonster(Globals.TrackedOffensiveEntity);
+            Globals._lastPosition = currentTargetPos;
+        }
     }
 
     public const string PluginVersion = "2.1.0";
