c8d42c7b2cc2aab8aca58841e5d85c0f820969aa
Chris Elsworth <chris@cae.me.uk>
2026-06-12 14:38:04 +0100

    re-remove difficulty

--- ShalazamPlugin/Extensions/EntityNpcGameObjectExtensions.cs
+++ ShalazamPlugin/Extensions/EntityNpcGameObjectExtensions.cs
@@ -56,21 +56,10 @@ public static class EntityNpcGameObjectExtensions
                 LocX = MathF.Round(entityNpcGameObject.transform.position.x, 2),
                 LocY = MathF.Round(entityNpcGameObject.transform.position.y, 2),
                 LocZ = MathF.Round(entityNpcGameObject.transform.position.z, 2),
-                Difficulty = GetDifficultyLabelForTier(entityNpcGameObject.info.Tier),
                 MaxHp = (int)entityNpcGameObject.Pools.GetPool(PoolType.Health).Max
             }
         };
     }
 
-    private static string GetDifficultyLabelForTier(NpcTier tier)
-    {
-        return tier switch
-        {
-            NpcTier.None => "solo",
-            NpcTier.Tier1 => "group",
-            NpcTier.Tier2 => "raid",
-            NpcTier.Tier3 => "gigachad",
-            _ => throw new ArgumentOutOfRangeException(nameof(tier), tier, null)
-        };
-    }
+
 }
