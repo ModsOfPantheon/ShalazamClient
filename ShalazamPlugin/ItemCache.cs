963af96770b2fc629f1826794ea66400709b89e0
Chris Elsworth <chris@cae.me.uk>
2026-07-01 22:24:59 +0100

    Remove unused methods

--- ShalazamPlugin/ItemCache.cs
+++ ShalazamPlugin/ItemCache.cs
@@ -8,31 +8,8 @@ public static class ItemCache
     // already uploaded from one place isn't re-sent when it shows up somewhere else.
     private static readonly HashSet<int> SeenItemIds = new();
 
-    public static void OnItemAdded(Item item, InventoryWithPersyst.AddFlags flags)
-    {
-        if (!SeenItemIds.Add(item.Template.ItemId))
-        {
-            return;
-        }
-
-        ModMain.ShalazamClient.PostItem(item);
-    }
-
-    public static void OnItemStackSizeChanged(Item item, int arg2, int arg3)
-    {
-        // Do not use the stack size, it has not been unboxed correctly and will return the memory address of
-        // the stack size instead... another bug with the il2cppinterop
-
-        if (!SeenItemIds.Add(item.Template.ItemId))
-        {
-            return;
-        }
-
-        ModMain.ShalazamClient.PostItem(item);
-    }
-
-    // Like OnItemAdded, but for Item instances discovered outside inventory (e.g. loot windows, quest
-    // reward slots).
+    // Called for every Item we come across (inventory, vendors, loot windows, quest reward slots). Uploads
+    // the first time we see a given ItemId and ignores it thereafter.
     public static void OnItemSeen(Item item)
     {
         if (item?.Template == null)
